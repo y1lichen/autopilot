b68596de27e23f27a0177e8fca56a23ad9239423
Jefferson <f1jefferson2@gmail.com>
2023-09-16 21:47:42 +0800

    manual mode added

---
+++ main.py
@@ -0,0 +1,57 @@
+import carla
+import random
+
+from manual_control import ManualControl
+
+
+class Main:
+    def __init__(self, manual_control=False, debug=False):
+        self.manual_control = manual_control
+        self.debug = debug
+
+        self.client = carla.Client("localhost", 2000)
+        self.world = self.client.get_world()
+
+        # world = client.load_world("Town06")
+        weather = carla.WeatherParameters(
+            cloudiness=0.0,
+            precipitation=0.0,
+            sun_altitude_angle=10.0,
+            sun_azimuth_angle=70.0,
+            precipitation_deposits=0.0,
+            wind_intensity=0.0,
+            fog_density=0.0,
+            wetness=0.0,
+        )
+        self.world.set_weather(weather)
+
+        bp_lib = self.world.get_blueprint_library()
+        spawn_points = self.world.get_map().get_spawn_points()
+
+        vehicle_bp = bp_lib.find("vehicle.audi.etron")
+        self.ego_vehicle = self.world.try_spawn_actor(vehicle_bp, spawn_points[79])
+
+        self.spectator = self.world.get_spectator()
+        self.transform = carla.Transform(
+            self.ego_vehicle.get_transform().transform(carla.Location(x=-4, z=2.5)),
+            self.ego_vehicle.get_transform().rotation,
+        )
+        self.spectator.set_transform(self.transform)
+
+        for _ in range(70):
+            vehicle_bp = random.choice(bp_lib.filter("vehicle"))
+            npc = self.world.try_spawn_actor(vehicle_bp, random.choice(spawn_points))
+
+        for v in self.world.get_actors().filter("*vehicle*"):
+            v.set_autopilot(True)
+        self.ego_vehicle.set_autopilot(False)
+
+    def play(self):
+        if self.manual_control:
+            manualControl = ManualControl(self.ego_vehicle, self.world, self.debug)
+            manualControl.play()
+
+
+if __name__ == "__main__":
+    main = Main(manual_control=True, debug=True)
+    main.play()
---
+++ manual_control.py
@@ -0,0 +1,63 @@
+import carla
+import pygame
+
+
+class ManualControl:
+    def __init__(self, vehicle, world, debug):
+        self.vehicle = vehicle
+        self.world = world
+        self.debug = debug
+
+        self.control = carla.VehicleControl()
+
+    def play(self):
+        pygame.init()
+        size = (360, 360)
+        pygame.display.set_caption("CARLA Manual Control")
+        pygame.display.set_mode(size)
+        clock = pygame.time.Clock()
+        done = False
+        while not done:
+            keys = pygame.key.get_pressed()
+
+            self.goFroward(keys)
+            self.goBackward(keys)
+            self.turn(keys)
+            self.hand_brake(keys)
+
+            self.world.tick()
+            # Update the display and check for the quit event
+            pygame.display.flip()
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
+
+            # Sleep to ensure consistent loop timing
+            clock.tick(60)
+
+    def goFroward(self, keys):
+        if keys[pygame.K_UP] or keys[pygame.K_w]:
+            self.control.throttle = min(self.control.throttle + 0.05, 1.0)
+        else:
+            self.control.throttle = 0.0
+        self.vehicle.apply_control(self.control)
+
+    def goBackward(self, keys):
+        if keys[pygame.K_DOWN] or keys[pygame.K_s]:
+            self.control.brake = min(self.control.brake + 0.2, 1.0)
+        else:
+            self.control.brake = 0.0
+
+    def turn(self, keys):
+        if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            self.control.steer = max(self.control.steer - 0.05, -1.0)
+        elif keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            self.control.steer = min(self.control.steer + 0.05, 1.0)
+        else:
+            self.control.steer = 0.0
+        self.vehicle.apply_control(self.control)
+
+    def hand_brake(self, keys):
+        self.control.hand_brake = keys[pygame.K_SPACE]
+        self.vehicle.apply_control(self.control)
